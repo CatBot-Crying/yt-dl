3cac72b958af0d0e2b8af3745ee5e73679e893f6
CatBot-Crying <88854387+CatBot-Crying@users.noreply.github.com>
2025-08-27 23:55:41 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // ตรวจสอบว่าโมดูลที่ import เข้ามา มาจากโฟลเดอร์ node_modules หรือไม่
-          if (id.includes('node_modules')) {
-            // ถ้าใช่, ให้จับกลุ่มโมดูลทั้งหมดที่มาจาก node_modules
-            // ไปไว้ในไฟล์ chunk เดียวที่ชื่อว่า 'vendor'
-            return 'vendor';
-          }
-        },
-      },
-    },
-  },
 })
