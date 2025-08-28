7580a9c7803d8c5252bb4be847dc3e540eee0821
CatBot-Crying <88854387+CatBot-Crying@users.noreply.github.com>
2025-08-28 09:53:19 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // ถ้ามีการเรียก API ไปที่ /api ให้ส่งต่อไปยัง PHP server
-      '/api': {
-        target: 'http://alone-value.atwebpages.com', // หรือ URL ของ PHP server ของคุณ เช่น http://localhost:8080
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
